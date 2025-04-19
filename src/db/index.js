0e60112c1613345fc0363a910e542fde56ea3ae2
Dev Garg <gargdev010300@gmail.com>
2025-04-19 18:30:54 +0530

    coded utils

--- src/db/index.js
+++ src/db/index.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
-import {DB_NAME} from "../constants.js";
+import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        console.log("MongoDB connected:", connectionInstance, connectionInstance.connection.host);
+        console.log("MongoDB connected:", connectionInstance.connection.host);
     }catch (error) {
         console.log("Error connecting to MongoDB:", error.message);
         process.exit(1);
     }
-}
+}  // as this is a asynchronous function so it will return a promise
 
 export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
+import { app } from "./app.js"
 
 dotenv.config({
     path: "./env"
 })
 
 connectDB()
+.then(() => {
+
+    app.on("error", (error) => {
+        console.log("Some error occured : ", error);
+        throw error;
+    })
+
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.log("mongodb connection failed : " ,err)
+})
