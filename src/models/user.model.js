715c4477674f112440e326218b1eb04fd63fc59c
Dev Garg <gargdev010300@gmail.com>
2025-04-19 22:07:34 +0530

    debug 2

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -8,7 +8,7 @@ const userSchema = new Schema(
             type: String, 
             required: true,
             unique: true,
-            lowercasae: true,
+            lowercase: true,
             trim: true,
             index: true,
         }, 
@@ -17,7 +17,7 @@ const userSchema = new Schema(
             type: String, 
             required: true,
             unique: true,
-            lowercasae: true,
+            lowercase: true,
             trim: true,
         }, 
 
@@ -61,7 +61,7 @@ const userSchema = new Schema(
 userSchema.pre("save", async function (next) {
     if(!this.isModified("password")) return next();
 
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
